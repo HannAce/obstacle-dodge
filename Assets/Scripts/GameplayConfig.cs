a3922d3690a55a9bb9f4fa5094c7a426334f087c
Hannah Fisher <hannahtfisher@gmail.com>
2025-01-17 16:50:44 +0000

    Added prefabs for some objects

--- Assets/Scripts/GameplayConfig.cs
+++ Assets/Scripts/GameplayConfig.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "GameplayConfig", menuName = "Hannah/GameplayConfig")]
+// Create the GameplayConfig in Unity by right clicking and going to whatever you chose to name the menu option
+[CreateAssetMenu(fileName = "GameplayConfig", menuName = "Configs/GameplayConfig")]
 public class GameplayConfig : ScriptableObject
 {
 
